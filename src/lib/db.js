4b445e5c4915e1e4550819a77e3b562ac313de79
Jair-Noto <jairnotoenriquez1@gmail.com>
2026-02-04 23:40:22 -0400

    Base de datos para vercel

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,21 @@
 import postgres from 'postgres';
 
-const sql = postgres({
-  host: import.meta.env.DB_HOST,
-  database: import.meta.env.DB_NAME,
-  username: import.meta.env.DB_USER,
-  password: import.meta.env.DB_PASSWORD,
-  port: 5432,
-  ssl: import.meta.env.PROD ? 'require' : false, // En producción (servidor) usa SSL
-  idle_timeout: 20, // Cierra conexiones inactivas para no saturar el servidor
-});
+// Definimos la conexión
+const sql = import.meta.env.DATABASE_URL
+  ? // 1. MODO NUBE (Vercel/Neon): Usa la URL larga y SSL obligatorio
+    postgres(import.meta.env.DATABASE_URL, {
+      ssl: 'require',
+      idle_timeout: 20
+    })
+  : // 2. MODO LOCAL (Tu PC): Usa las variables sueltas como antes
+    postgres({
+      host: import.meta.env.DB_HOST,
+      database: import.meta.env.DB_NAME,
+      username: import.meta.env.DB_USER,
+      password: import.meta.env.DB_PASSWORD,
+      port: 5432,
+      ssl: false,
+      idle_timeout: 20
+    });
 
 export default sql;
